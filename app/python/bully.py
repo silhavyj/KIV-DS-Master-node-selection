effdb7bf8e5c1d7d09bf6bd4e8c6d9b9b1ebece5
silhavyj <jakub.sil@seznam.cz>
2022-10-05 17:51:39 +0200

    backup

--- app/python/bully.py
+++ app/python/bully.py
@@ -17,8 +17,31 @@ class Bully:
         self.master_ip_addr = None
         self.color = 'GRAY'
         self.mtx = Lock()
+        self.color_counter = 0
 
         self.nodes = {}
+
+    def get_info(self):
+        return {
+            'name'     : self.network_info.hostname,
+            'node_id'  : str(self.node_id),
+            'master'   : str(self.master),
+            'election' : str(self.election),
+            'ip_addr'  : str(self.network_info.interface.ip),
+            'color'    : self.color
+        }
+
+    def calculate_node_color(self):
+        color = None
+        if self.color_counter < 2:
+            color = 'GREEN'
+        else:
+            color = 'RED'
+
+        self.mtx.acquire()
+        self.color_counter = (self.color_counter + 1) % 3
+        self.mtx.release()
+        return color
 
     
     def add_node(self, ip_addr, data):
@@ -43,16 +66,18 @@ class Bully:
         self.mtx.acquire()
         self.color = color
         self.mtx.release()
+        logging.info(f"Color has been changed to '{self.color}'")
 
     
-    def discover_other_nodes(self, max_nodes):
+    def discover_other_nodes(self, max_nodes, skip_itself=False):
         logging.info('Scanning the network...')
         
         count = 0
         master_ip_addr = None
 
         for ip_addr in self.network_info.interface.network:
-            if ip_addr == self.network_info.interface.ip:
+            
+            if skip_itself == True and ip_addr == self.network_info.interface.ip:
                 logging.info(f'Skipping {ip_addr} (local machine)')
                 continue
 
@@ -65,11 +90,11 @@ class Bully:
                     data = response.json()
                     if data['master'] == 'True':
                         if master_ip_addr is None:
-                            master_ip_addr = data['ip_addr']
+                            master_ip_addr = ip_addr
                             self.set_master_ip_addr(master_ip_addr)
                             logging.info(f'Master found - {master_ip_addr}')
                         else:
-                            logging.critical(f"Two or more masters found on this network - {master_ip_addr} and {data['ip_addr']}. Exiting...")
+                            logging.critical(f"Two or more masters found on the network - {master_ip_addr} and {ip_addr}. Exiting...")
                             sys.exit(1)
                     self.add_node(ip_addr, data)                    
                 else:
@@ -81,8 +106,19 @@ class Bully:
             if count == max_nodes:
                 break
 
+        logging.info('Finished scanning the network')
+
         if len(self.nodes) == 0:
             self.set_master(True)
-            logging.info('No other nodes have been found on this network -> becoming the master')
-        
-        logging.info('Finished scanning the network')
+            self.set_color('GREEN')
+            logging.info('No other nodes have been found on the network -> becoming the master')
+        elif self.master == False:
+            logging.info('Registering with the master')
+            api = f'http://{ip_addr}:5000/worker_register'
+            response = requests.post(api, json=self.get_info(), verify=False, timeout=0.5)
+            if response.status_code != 200:
+                logging.error('Failed to register with the master. Exiting...')
+                sys.exit(2)
+            self.set_color(response.json()['color'])
+
+            # TODO start pinging the master
--- app/python/main.py
+++ app/python/main.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from threading import Thread
 from flask import Flask, request, jsonify
@@ -8,45 +9,52 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
-bully = Bully('eth1')
+bully = Bully('enp0s25')
 
-network_scan = Thread(target=bully.discover_other_nodes, args=(5,))
+network_scan = Thread(target=bully.discover_other_nodes, args=(18,))
 network_scan.start()
 
 @app.route('/node-details', methods=['GET'])
 def get_details():
-    return jsonify({
-        'name'      : bully.network_info.hostname,
-        'node_id'   : str(bully.node_id),
-        'master'    : str(bully.master),
-        'election'  : str(bully.election),
-        'interface' : str(bully.network_info.interface.ip),
-    })
+    return jsonify(bully.get_info()), 200
 
 
 @app.route('/color', methods=['POST'])
 def set_color():
     if bully.master == True:
         return jsonify({
-            'Response': 'ERROR',
-            'Message' : 'Cannot set the color of the master'
+            'response' : 'ERROR',
+            'message'  : 'Cannot set the color of the master'
         }), 400
 
     data = request.get_json()
+
     if 'color' in data:
         bully.set_color(data['color'])
-        logging.info(f"color has been changed to '{bully.color}'")
         return jsonify({'Response': 'OK'}), 200
     
     return jsonify({
-        'Response': 'ERROR',
-        'Message' : 'Invalid data (color is missing)'
+        'response' : 'ERROR',
+        'message'  : 'Invalid data (color is missing)'
     }), 400
 
 
 @app.route('/worker_register', methods=['POST'])
 def worker_register():
-    pass
+    if bully.master == False:
+        return jsonify({
+            'response' : 'ERROR',
+            'message'  : 'I am not the master on the network'
+        }), 400
+
+    data = request.get_json()
+    logging.info(f"Node {data['ip_addr']} has registered with the master")
+    
+    bully.add_node(data['ip_addr'], data)
+    return jsonify({
+        'response' : 'OK',
+        'color'    : bully.calculate_node_color() 
+    }), 200
 
 
 @app.route('/master_announcement', methods=['POST'])
@@ -55,4 +63,4 @@ def register_new_node():
 
 
 if __name__ == '__main__':
-    app.run(host=str(bully.network_info.interface.ip))
+    app.run(host=str(bully.network_info.interface.ip), port=int(sys.argv[1]))
