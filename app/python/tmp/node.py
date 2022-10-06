4ca3dbfc20c5f50882456d98b3b716009a1869f9
silhavyj <jakub.sil@seznam.cz>
2022-10-06 20:48:17 +0200

    backup

---
+++ app/python/tmp/node.py
@@ -0,0 +1,53 @@
+import netifaces as ni
+from threading import Thread, Lock
+
+from logger import log
+
+class Node:
+
+    def __init__(self, interface_name):
+        self._election = False
+        self._is_master = False
+        self._master_ip_addr = None
+        self._lock = Lock()
+        self._nodes = []
+        self._color = None
+
+        interface_info = ni.ifaddresses(interface_name)[ni.AF_INET][0]
+        ip_addr = interface_info['addr']
+        netmask = interface_info['netmask']
+        self._interface = IPv4Interface(f'{ip_addr}/{netmask}')
+
+
+    def get_details(self):
+        return {
+            'is_master' : self._is_master,
+            'color'     : self._color
+        }
+
+
+    def set_election_flag(self, value):
+        self._lock.acquire()
+        self._election = value
+        self._lock.release()
+
+
+    def add_node(self, ip_addr):
+        self._lock.acquire()
+        if ip_addr not in self._nodes:
+            self._nodes.append(ip_addr)
+        self._lock.release()
+
+    
+    def set_color(self, value):
+        self._lock.acquire()
+        self._color = value
+        self._lock.release()
+        log.info(f'The color has been changed to {value}')
+
+    
+    def set_is_master(self):
+        self._lock.acquire()
+        self._is_master = True
+        self._lock.release()
+        log.info(f'This node ({self._interface.ip} has now become the master)')
